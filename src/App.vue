8aecc09fca09c6882f3c0aac9c5a035a7b912432
ThomasBentes <thomas_brayan10@hotmail.com>
2021-09-28 12:53:51 -0300

    Ultimas atualizações

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
   <v-app> 
     <v-main>
       <v-container>
-        <div class="logo" @click="goHome">
-        <v-img src="./assets/Singular.png" width="30%" style="margin-rigth:10dp"></v-img>
+        <div class="logo" >
+        <v-img id="se" src="./assets/Singular.png" width="30%" style="margin-rigth:10dp" @click="goHome"></v-img>
         <h1 class="saudation">Saudações, Estudante...</h1>
         <h2 class="question">Diga-nos, para onde você quer ir hoje?</h2>
         </div>
       </v-container>
 
-      <v-container class="superior">
-          <div class="geral">
+      <v-container>
+          <div class="geral" style="display:flex; flex-direction:row; flex-wrap: nowrap; align-conten: center; justify-content: center; align-items: center;">
             <div class="card__container">
               <div class="card">
                 <div class="card__content" @click="goBills">
@@ -79,40 +79,42 @@ main{
   margin-top: 10px;
 }
 .card {
-  width: 80%;
-  height: 130%;
-  max-height: 300px;
+  width: 60%;
+  height: 80%;
+  max-height: 300px;  
   padding: auto;
   text-align: center;
-  background: #242426;
+  //background: #242426;
   border-radius: 30px;
-  box-shadow: 10px 10px 25px #1b1c1b, -10px -10px 25px #2d302f;
+  //box-shadow: 5px 5px 5px #1b1c1b, -5px -5px 5px #2d302f;
   &__container {
-    display: grid;
-    width:100%;
-    height:120%;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    display: flex;
+    width:60%;
+    height: 60%;
+    grid-template-columns: 1fr 1fr;
     align-items: center;
     justify-items: center;
     color: rgb(0, 0, 0);
+    
   }
   &__content {
-    width: 100%;
-    height: 110%;
+    // box-shadow: -5px -5px 5px #444444;
+    width: 90%;
+    height: 90%;
     background: whitesmoke;
     margin: 10px auto;
     border-radius: 30px;
-    padding: 10px;
+    padding: 20px;
     cursor: pointer;
     transition: 0.3s all ease-in-out;
     &:hover{
-      margin-top:-10px;
+      margin-top:-40px;
     }
   }
   &__header {
     text-transform: uppercase;
     font-size: 30px;
-    margin: 30px auto;
+    margin: 5px auto;
   }
   .boleto{
     width: 150px;
@@ -120,6 +122,5 @@ main{
   .portal{
     width: 170px;
   }
-  
 }
 </style>

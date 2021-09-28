a62f67e92552421c34c102fc8b0e67722c5879d7
ThomasBentes <thomas_brayan10@hotmail.com>
2021-09-28 11:24:46 -0300

    Troca de Card

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,30 @@
   <v-app> 
     <v-main>
       <v-container>
-        <div class="logo">
+        <div class="logo" @click="goHome">
         <v-img src="./assets/Singular.png" width="30%" style="margin-rigth:10dp"></v-img>
         <h1 class="saudation">Saudações, Estudante...</h1>
         <h2 class="question">Diga-nos, para onde você quer ir hoje?</h2>
         </div>
       </v-container>
 
-      <v-container>
-        <div>
-          <v-row dense justify="space-between" >
-            <v-col
-              lg="3"
-              class=" pa-2 mt-5 mb-8"
-            >
-              <v-card
-                outlined
-                tile
-                @click="goBills"
-                style="text-align: -webkit-center;"
-              >
-                <v-card-title style="font-size: 45px; place-content: center;">Boleto</v-card-title>
-                <v-img src="./assets/ic boleto.png" width="300px"></v-img>
-              </v-card>           
-            </v-col>
-            <v-col
-              lg="3"
-              class="pa-2 mt-5 mb-8"
-            >
-              <v-card
-                outlined
-                tile
-                style="text-align: -webkit-center;"
-                @click="goPortal"
-              >
-                <v-card-title style="font-size: 40px; place-content: center;">Portal do Aluno</v-card-title>
-                <v-img src="./assets/ic portal.png" width="330px"></v-img>
-              </v-card>           
-            </v-col>
-          </v-row>
-        </div>   
+      <v-container class="superior">
+          <div class="geral">
+            <div class="card__container">
+              <div class="card">
+                <div class="card__content" @click="goBills">
+                  <h3 class="card__header">Boleto</h3>
+                  <img src="./assets/ic boleto.png" class="boleto">
+                </div>
+              </div>
+              <div class="card">
+                <div class="card__content" @click="goPortal">
+                  <h3 class="card__header">Portal do Aluno</h3>
+                  <img src="./assets/ic portal.png" class="portal">
+                </div>
+              </div>
+            </div>
+          </div> 
       </v-container>
     </v-main>
   </v-app>
@@ -62,16 +46,20 @@ export default {
   }),
   methods:{
     goBills(){
-      location.href = "https://www.youtube.com/results?search_query=clicable+v-card";
+      location.href = "https://portal.bancomonetiza.com.br/app/consultar-boleto";
     },
     goPortal(){
-      location.href = "https://www.facebook.com/?ref=logo"
+      location.href = "https://portal.singulareducacional.com.br/rm/web/app/edu/portaleducacional/login/"
+    },
+    goHome(){
+      location.href="https://www.singulareducacional.com.br/"
     }
     
   }
 };
 </script>
 <style lang="scss">
+
 main{
   background-image: url(./assets/Background.png);
   background-size: 100%;
@@ -89,5 +77,49 @@ main{
   color: white;
   font-size: 40px;
   margin-top: 10px;
+}
+.card {
+  width: 80%;
+  height: 130%;
+  max-height: 300px;
+  padding: auto;
+  text-align: center;
+  background: #242426;
+  border-radius: 30px;
+  box-shadow: 10px 10px 25px #1b1c1b, -10px -10px 25px #2d302f;
+  &__container {
+    display: grid;
+    width:100%;
+    height:120%;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    color: rgb(0, 0, 0);
+  }
+  &__content {
+    width: 100%;
+    height: 110%;
+    background: whitesmoke;
+    margin: 10px auto;
+    border-radius: 30px;
+    padding: 10px;
+    cursor: pointer;
+    transition: 0.3s all ease-in-out;
+    &:hover{
+      margin-top:-10px;
+    }
+  }
+  &__header {
+    text-transform: uppercase;
+    font-size: 30px;
+    margin: 30px auto;
+  }
+  .boleto{
+    width: 150px;
+  }
+  .portal{
+    width: 170px;
+  }
+  
 }
 </style>
